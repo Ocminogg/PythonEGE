332ebe9234682b53f72ad9549ab047b8b7582142
Александр Куршнев <a.kurshnev@xlab-game.com>
2023-12-21 18:11:14 +0300

    26 dodelatm zadachu pro zakupku tovarov

--- zadanie 26 EGE.py
+++ zadanie 26 EGE.py
@@ -1,4 +1,4 @@
-# docum = open('26 (1).txt').readline()
+# docum = open('28132.txt').readline()
 # docum = docum.split(' ')
 # Varhive = int(docum[0])
 # Users = int(docum[1])
@@ -83,15 +83,16 @@
 # print(docum)
 # print(Zagruzili)
 
-docum = open('26 (1).txt').readline()
+docum = open('26.txt').readline()
 docum = docum.split(' ')
 TovaraNaSklade = int(docum[0])
 Money = int(docum[1])
 
-docum = open('26 (1).txt').readlines()
+docum = open('26.txt').readlines()
 docum.remove(docum[0])
 
 print(docum)
+MASSIV_A = []
 MASSIV_B = []
 for i in range(len(docum)):
     docum[i] = docum[i].split(' ', 3)
@@ -100,8 +101,81 @@ for i in range(len(docum)):
     del docum[i][3]
     if docum[i][2] == "B":
         MASSIV_B.append(docum[i])
+    if docum[i][2] == "A":
+        MASSIV_A.append(docum[i])
 print(docum)
+print(MASSIV_A)
 print(MASSIV_B)
 
+MASSIV_A.sort()
+print(MASSIV_A)
 # docum.sort()
 # print(docum)
+
+# docum = open('28132.txt').readline()
+# docum = docum.split(' ')
+# Varhive = int(docum[0])
+# Users = int(docum[1])
+# docum = open('28132.txt').readlines()
+# docum.remove(docum[0])
+#
+# print(docum)
+#
+# for i in range(len(docum)):
+#     docum[i] = int(docum[i])
+#
+# print(docum)
+# docum.sort()
+# print(docum)
+#
+# UsersV = 0
+# count = 0
+# for i in docum:
+#     if UsersV <= Varhive:
+#         UsersV += i
+#         count += 1
+#     else:
+#         count -= 1
+#         UsersV = UsersV - int(docum[count])
+#         print(docum[count])
+#         print(count)
+#         print(UsersV)
+#         break
+#
+# UsersV = UsersV - int(docum[count-1])
+# zapas = Varhive - UsersV
+# print("Конец")
+# print(docum[count - 1])
+# print(UsersV)
+# print(zapas)
+# MAX = 0
+# for i in docum:
+#     if (i <= zapas) and (i > MAX):
+#         MAX = i
+#     elif i > zapas:
+#         break
+# print("Ответ")
+# print(MAX)
+# print(count)
+
+# f = open('28132.txt')
+# data = f.readlines()
+# s = data[0].split()
+# s = int(s[0])
+# del (data[0])  # первая строка больше не нужна, удаляем ее
+# for i in range(0, len(data)):
+#     data[i] = int(data[i])
+# data = sorted(data)
+# summa = 0
+# for count in range(0, len(data)):
+#     if summa + data[count] > s: break
+#     summa += data[count]
+# print(count)
+# zapas = s - summa
+# print("summa " + str(summa))
+# print("zapas " + str(zapas))
+# print("data[count] " + str(data[count]))
+# for i in range(0, len(data)):
+#     if data[i] - data[count - 1] <= zapas:
+#         itog = data[i]
+# print(itog)
